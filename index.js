6866d7f9a0dbedd877db04f0ebf28930f2d3228b
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-11 17:26:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Home route 
+app.get('/',(req,res)=>{
+    res.send("You are at home route")
+})
+// app.use express.json
+app.use(express.json());
+// Home route with post method
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.send("You are at home with  post method")
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening at localhost:${PORT}`)
+})
