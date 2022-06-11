1bb5d1c28271bb9fab70a96eea652a0bbc427bf6
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-11 18:20:34 +0600

    Express static

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ app.get('/',(req,res)=>{
     res.send("You are at home route")
 })
 // app.use express.json
-app.use(bodyParser.urlencoded({ extended: true }))
-// 
-// app.use(bodyParser.json())
+app.use(express.static(__dirname + "/public/"));
 // Home route with post method
 app.post('/',(req,res)=>{
     console.log(req.body);
