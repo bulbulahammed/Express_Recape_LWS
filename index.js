876d6aede8c6550ed3fe151521580796a7340a7e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-15 00:45:19 +0600

    hostname is not changable

--- index.js
+++ index.js
@@ -7,21 +7,16 @@ const adminRoute = express.Router();
 
 // Admin route
 adminRoute.get("/dashboard",(req,res)=>{
-    console.log(req.baseUrl);
+    console.log(req.hostname);
     res.send("You are at Admin route");
 })
 // Home route 
 app.get('/',(req,res)=>{
-    console.log(req.baseUrl)
+    console.log(req.hostname)
     res.send("You are at home route")
 })
 // 
 app.use('/admin',adminRoute)
-// Home route with post method
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send("You are at home with  post method")
-})
 
 app.listen(PORT,()=>{
     console.log(`App is listening at localhost:${PORT}`)
