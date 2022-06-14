4c2d5c0a00f25332bbdddc28d5e57bb4ce2de122
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-15 00:19:02 +0600

    base url and sub route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const adminRoute = express.Router();
 
 // Admin route
 adminRoute.get("/dashboard",(req,res)=>{
+    console.log(req.baseUrl);
     res.send("You are at Admin route");
 })
 // Home route 
@@ -14,8 +15,8 @@ app.get('/',(req,res)=>{
     console.log(req.baseUrl)
     res.send("You are at home route")
 })
-// app.use express.json
-app.use(express.json());
+// 
+app.use('/admin',adminRoute)
 // Home route with post method
 app.post('/',(req,res)=>{
     console.log(req.body);
