7ff891d35ac308977091ff46dbdcd84185731d1d
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-15 13:38:43 +0600

    err handler middleware

--- index.js
+++ index.js
@@ -3,25 +3,18 @@ const app = express();
 const PORT = 3000;
 
 
-const adminRoute = express.Router();
-
-// Admin route
-adminRoute.get("/dashboard",(req,res)=>{
-    console.log(req.baseUrl);
-    res.send("You are at Admin route");
-})
 // Home route 
 app.get('/',(req,res)=>{
     console.log(req.baseUrl)
     res.send("You are at home route")
 })
-// 
-app.use('/admin',adminRoute)
-// Home route with post method
-app.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send("You are at home with  post method")
-})
+
+const errHandler = (err,req,res,next)=>{
+    console.log(err);
+    res.status(500).send("There is a problem in server side");
+};
+
+app.use(errHandler);
 
 app.listen(PORT,()=>{
     console.log(`App is listening at localhost:${PORT}`)
