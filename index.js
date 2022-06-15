13eb64487d6a219d08fc8ffb70b9ed9a31054ff2
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-06-15 13:56:12 +0600

    Example of err handler middleware

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const bodyParser = require("body-parser");
 
 
-//  implement Body Parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-// 
-app.get("/register",(req,res)=>{
-    res.sendFile(__dirname + "/register.html");
+// Home route 
+app.get('/',(req,res)=>{
+    console.log(req.baseUrl)
+    res.send("You are at home route")
 })
 
+const errHandler = (err,req,res,next)=>{
+    console.log(err);
+    res.status(500).send("There is a problem in server side");
+};
 
-// Send html file to the backend with post method
-app.post("/register",(req,res)=>{
-    const name = req.body.name;
-    const age = req.body.age;
-    res.send(`<h2>Your Name: ${name}, age: ${age}.</h2>`);
-});
-
-app.get("/",(req,res)=>{
-    res.send("You are at Home Page");
-});
-
+app.use(errHandler());
 
 app.listen(PORT,()=>{
-    console.log(`Your Server is running at localhost:${PORT}`);
+    console.log(`App is listening at localhost:${PORT}`)
 })
-
-
-
